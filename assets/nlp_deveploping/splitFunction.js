4fbaa97db4aab3ec7e7e88a85f6defc58cddc439
18jkm20 <49576411+18jkm20@users.noreply.github.com>
2021-01-25 23:30:33 +0900

    v1

---
+++ assets/nlp_deveploping/splitFunction.js
@@ -0,0 +1,109 @@
+function analyseVocab() {
+
+  //checking result div box
+  let searchingResultBox = document.getElementById("resultDivBox");
+  if (searchingResultBox) {
+    // console.log("yes");
+    searchingResultBox.parentNode.removeChild(searchingResultBox);
+  } else {
+    // console.log("no");
+  }
+
+  //get text data from text area
+  let text = document.getElementById("myText").value.toString();
+
+  //analyse passage with nlp-compromise
+  let doc = nlp(text);
+
+  //seperate a word in passage with without check verb and noun form
+  let vocabs = doc.terms().data().map(x => x.text.replace(/[^\w ]/, ''));
+
+  //get only infinitive form of verb vocab in passage
+  let verbVocabs = doc.verbs().conjugate().map(x => x.Infinitive.replace(/[^\w ]/, ''));
+
+  //get only sigular form of noun vocab in passage
+  let nounVocabs = doc.nouns().data().map(x => x.singular.replace(/[^\w ]/, ''));
+
+
+  //combine vocabulary together in allVocab
+  nounVocabs.push(...verbVocabs);
+  nounVocabs.push(...vocabs);
+  let allVocabs = new Set(nounVocabs);
+
+
+  //create result div box
+  let resultDivBox = document.createElement('div');
+  resultDivBox.setAttribute("id", "resultDivBox");
+  document.body.appendChild(resultDivBox);
+
+  //select level data
+  let e = document.getElementById("levelNumber");
+  let levelNumber = Number(e.options[e.selectedIndex].text);
+
+
+
+  //select language data
+  let b = document.getElementById("languageMeaning");
+  let languageNumber = Number(b.options[b.selectedIndex].value);
+  if(languageNumber === 1){
+    //show meaning data
+    for (let i = levelNumber - 1; i < 12; i++) {
+      for (let vocabb of allVocabs) {
+
+        let vocab = vocabb.toLowerCase();
+        let numberIndex = vocabDatas.indexOf(vocab);
+
+        if (vocabDatas.slice(i * 1000, (i + 1) * 1000).includes(vocab)) {
+          let result = "";
+          let level = parseInt(numberIndex / 1000) + 1;
+
+          result = "level" + " " + level + " " + vocab + " : " + japaneseMeanDatas[numberIndex];
+
+
+          //show data in resultDivBox
+          let resultBox = document.querySelector("#resultDivBox");
+          let para = document.createElement("P");
+          para.innerHTML = result;
+          resultBox.appendChild(para);
+
+        } else {
+          // console.log(vocab);
+        }
+      }
+    }
+  }else if(languageNumber === 2){
+    //show meaning data
+    for (let i = levelNumber - 1; i < 12; i++) {
+      for (let vocabb of allVocabs) {
+
+        let vocab = vocabb.toLowerCase();
+        let numberIndex = vocabDatas.indexOf(vocab);
+
+        if (vocabDatas.slice(i * 1000, (i + 1) * 1000).includes(vocab)) {
+          let result = "";
+          let level = parseInt(numberIndex / 1000) + 1;
+
+          result = "level" + " " + level + " " + vocab + " : " + thaiMeanDatas[numberIndex];
+
+
+          //show data in resultDivBox
+          let resultBox = document.querySelector("#resultDivBox");
+          let para = document.createElement("P");
+          para.innerHTML = result;
+          resultBox.appendChild(para);
+
+        } else {
+          // console.log(vocab);
+        }
+      }
+    }
+  }
+
+
+}
+
+
+
+function refresh() {
+  window.location.reload();
+}
---
+++ assets/nlp_deveploping/splitFunction2.js
@@ -0,0 +1,41 @@
+
+/*
+
+//text pasage declare
+let text = "Thailand has begun three days of coronation ceremonies for King Maha Vajiralongkorn";
+// let text = "he will have been walking quickly";
+
+//analyze passage with nlp-compromise
+let doc = nlp(text);
+
+
+//declare variable array
+let allVocabs = [];
+
+//declare vocab term from passage
+let vocabs = doc.terms().out('frequency').map(x => x.normal);
+
+
+
+// console.log(doc.verbs().data());
+// console.log(doc.nouns().data());
+
+/*
+for(vocab of vocabs){
+    
+}
+*/
+
+
+
+/*
+sentence = "The doctors treated dogs.";
+wordArray = RiTa.tokenize(sentence);
+// wordArray = RiTa.tokenize(sentence, "\\s");
+
+console.log(wordArray);
+*/
+
+
+let a = ['a','b','c','d','e'];
+console.log(a.slice(0,5));
